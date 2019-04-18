b361c4a0b94626659c1c9762bbfdf8a7b985b366
Katherine Miao <katmiao99@gmail.com>
2019-04-17 21:14:27 -0700

    profile and explore

---
+++ Pulp/Pulp/ExploreResults.swift
@@ -0,0 +1,9 @@
+//
+//  ExploreResults.swift
+//  Pulp
+//
+//  Created by Katherine Miao on 3/14/19.
+//  Copyright © 2019 Katherine Miao. All rights reserved.
+//
+
+import Foundation
---
+++ Pulp/Pulp/PageCell.swift
@@ -0,0 +1,9 @@
+//
+//  PageCell.swift
+//  Pulp
+//
+//  Created by Katherine Miao on 3/14/19.
+//  Copyright © 2019 Katherine Miao. All rights reserved.
+//
+
+import Foundation

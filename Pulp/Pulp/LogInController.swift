3e0fc9cb3fd62f4e90c848bc08d984cefee1160d
Any Cai <andycai20000212@gmail.com>
2019-05-14 15:02:15 -0700

    Added customized Facebook Login button

--- Pulp/Pulp/LogInController.swift
+++ Pulp/Pulp/LogInController.swift
@@ -8,15 +8,70 @@
 
 import Foundation
 import FacebookLogin
+import SnapKit
 
 class LogInController : UIViewController {
     override func viewDidLoad() {
         view.backgroundColor = UIColor(rgb: 0xF7F7F7)
-        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
-        loginButton.center = view.center
+        setUpLoginButton()
+
+        view.addSubview(FBLoginButton)
+        view.addSubview(graphicView)
+        setUpAutoLayout()
         
-        view.addSubview(loginButton)
+//        if let accessToken = AccessToken.current {
+//            // User is logged in, use 'accessToken' here.
+//        }
     }
     
+    private let graphicView:UIImageView = {
+        let imageView = UIImageView(image:#imageLiteral(resourceName: "Fruits"))
+    
+        imageView.contentMode = UIView.ContentMode.scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let FBLoginButton : UIButton = {
+        let myLoginButton = UIButton(type: .custom)
+        myLoginButton.backgroundColor = UIColor.blue
+        myLoginButton.layer.cornerRadius = 12
+        myLoginButton.setTitle("My Login Button", for: .normal)
+        
+        return myLoginButton
+    }()
+    
+    func setUpLoginButton()
+    {
+        FBLoginButton.addTarget(self, action: #selector(self.loginButtonClicked),  for: .touchUpInside)
+    }
+    
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn(readPermissions: [ .publicProfile, .email, .userFriends ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success( _, _, _):
+                print("Logged in!")
+            }
+        }
+    }
+    
+    func setUpAutoLayout()
+    {
+        FBLoginButton.snp.makeConstraints({ (make) -> Void in
+            make.width.equalTo(180)
+            make.height.equalTo(40)
+            make.center.equalTo(self.view)
+        })
+        
+        graphicView.snp.makeConstraints { (make) -> Void in
+            make.right.equalTo(view)
+            make.top.equalTo(view)
+        }
+    }
     
 }
